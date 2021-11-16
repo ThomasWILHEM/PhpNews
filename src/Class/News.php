805b12376ad223195fa9778ca60b5ff002b6582f
ThomasWILHEM <thomaswilhempro@outlook.fr>
2021-11-16 19:30:38 +0100

    Modification de la classe News

--- src/Class/News.php
+++ src/Class/News.php
@@ -2,10 +2,87 @@
 
 class News
 {
-    private int $idNews;
     private string $titre;
     private string $description;
     private string $lien;
     private string $date;
+
+    /**
+     * @return string
+     */
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param string $titre
+     */
+    public function setTitre(string $titre): void
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLien(): string
+    {
+        return $this->lien;
+    }
+
+    /**
+     * @param string $lien
+     */
+    public function setLien(string $lien): void
+    {
+        $this->lien = $lien;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param string $date
+     */
+    public function setDate(string $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @param string $titre
+     * @param string $description
+     * @param string $lien
+     * @param string $date
+     */
+    public function __construct(string $titre, string $description, string $lien, string $date)
+    {
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->lien = $lien;
+        $this->date = $date;
+    }
 
 }
