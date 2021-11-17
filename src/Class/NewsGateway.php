c0a7f04a4fbc8a07850e1cd7a9b6f845ea0128ae
Florent MARQUES <florent.marques@etu.uca.fr>
2021-11-17 20:26:03 +0100

    Ajout de la gateway pour les news

---
+++ src/Class/NewsGateway.php
@@ -0,0 +1,41 @@
+<?php
+require_once ("Connection.php");
+class NewsGateway
+{
+    private $con;
+
+    public function __construct(Connection $c)
+    {
+        $this->con=$c;
+    }
+
+    public function insert(string $titre, string $description, string $lien, string $date){
+        $query='INSERT INTO news VALUES(:titre,:description,:lien,:date);';
+        $this->con->executeQuery($query,array(
+            ':titre'=> array($titre,PDO::PARAM_STR),
+            ':description'=> array($description,PDO::PARAM_STR),
+            ':lien'=> array($lien,PDO::PARAM_STR),
+            ':date'=> array($date,PDO::PARAM_STR)
+        ));
+    }
+
+    public function delete(string $lien){
+        $query='DELETE FROM news WHERE lien=:lien';
+        $this->con->executeQuery($query,array(
+            ':lien'=> array($lien,PDO::PARAM_STR)
+        ));
+    }
+
+    public function update(string $lien,array $parameters){
+        $query='UPDATE news SET  WHERE lien=:lien';
+        $this->con->executeQuery($query,array(
+            ':lien'=> $parameters
+        ));
+    }
+
+    public function findAllNews(){
+        $query='SELECT * FROM news';
+        $this->con->executeQuery($query);
+    }
+
+}
