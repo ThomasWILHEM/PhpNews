1bc504398792de34eea437893e18d80863e1a175
flomSStaar <63flotoof@gmail.com>
2021-11-18 00:20:44 +0100

    Ajout de la partie html

---
+++ src/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Accueil - PhpNews</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+        <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+        <link rel="stylesheet" href="assets/css/index.css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+            <div class="container"><a class="navbar-brand logo" href="">PhpNews</a><button data-bs-toggle="collapse"
+                    class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle
+                        navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link active" href="index.html">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="login.html">Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="page landing-page">
+            <ul class="news-list">
+                <li class="news-item white">
+                    <p class="news-date">17/11-21:58:03</p>
+                    <a href="" class="news-title news-link">Titre 2</a>
+                    <a href=""><img src="assets/img/pikachu.gif" class="news-img"></a>
+                    <a class="news-desc news-link" href="">Ma super
+                        ddsd jdkjsqlk djqslkdj qslkdj qslkdjqslkdj qslkdj sqd qsd q
+                        qslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkd
+                        jqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjes
+                    </a>
+                </li>
+                <li class="news-item white">
+                    <p class="news-date">17/11-21:58:03</p>
+                    <a href="" class="news-title news-link">Titre 2</a>
+                    <a href=""><img src="assets/img/pikachu.gif" class="news-img"></a>
+                    <a class="news-desc news-link" href="">Ma super
+                        ddsd jdkjsqlk djqslkdj qslkdj qslkdjqslkdj qslkdj sqd qsd q
+                        qslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkd
+                        jqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjes
+                    </a>
+                </li>
+                <li class="news-item white">
+                    <p class="news-date">17/11-21:58:03</p>
+                    <a href="" class="news-title news-link">Titre 2</a>
+                    <a href=""><img src="assets/img/pikachu.gif" class="news-img"></a>
+                    <a class="news-desc news-link" href="">Ma super
+                        ddsd jdkjsqlk djqslkdj qslkdj qslkdjqslkdj qslkdj sqd qsd q
+                        qslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkd
+                        jqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjesqslkdjqlskdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjqslkdjlkjes
+                    </a>
+                </li>
+                
+            </ul>
+        </main>
+        <footer
+            class="d-flex d-lg-flex justify-content-center align-items-center justify-content-lg-center align-items-xl-end page-footer dark">
+            <p class="d-lg-flex justify-content-lg-center" style="text-align: center;color: rgb(255,255,255);">Florent
+                MARQUES - G1 - Thomas WILHEM</p>
+        </footer>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+        <script src="assets/js/vanilla-zoom.js"></script>
+        <script src="assets/js/theme.js"></script>
+    </body>
+
+</html>
