6464bd028582175ebad17a1d3e84dae2cf52c63e
flomSStaar <63flotoof@gmail.com>
2021-12-03 23:01:52 +0100

    Modification de la vue admin. Style du bouton déconnexion. Fenêtre modale pour l'ajout d'un flux RSS. Correction de bugs (responsive)

--- src/Vues/admin.php
+++ src/Vues/admin.php
@@ -23,7 +23,8 @@
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
                 <li class="nav-item"><a class="nav-link active" href="index.php?action=admin">Admin</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?action=deconnexion">Déconnexion</a></li>
+                <li class="nav-item"><a class="nav-link btn btn-primary text-white" href="index.php?action=deconnexion">Déconnexion</a>
+                </li>
             </ul>
         </div>
     </div>
@@ -32,34 +33,65 @@
     <h2 class="adminTitle">Page admin</h2>
     <div class="m20-0">
         <h4 class="text-decoration-underline">Nombre de news</h4>
-        <form method="post">
-            <label for="newsnumber">Nombre de news à afficher par page :</label>
-            <input id="newsnumber" name="newsnumber" type="number" min="1" value="10" required>
-            <input type="submit" value="Enregistrer">
+        <form class="row g-3 align-items-center" method="post">
+            <div class="col-auto">
+                <label for="newsnumber">Nombre de news à afficher par page :</label>
+            </div>
+            <div class="col-auto hstack gap-3">
+                <input class="form-control" id="newsnumber" name="newsnumber" type="number" min="1" value="10" required>
+                <input class="btn btn-primary" type="submit" value="Enregistrer">
+            </div>
             <input type="hidden" name="action" value="modifierNbNews">
         </form>
     </div>
 
-    <div class="m20-0">
-        <h4 class="text-decoration-underline"> Ajouter un site </h4>
-        <form method="post">
-            <label for="nomsite">Nom du site : </label>
-            <input style="margin: 0 20px 0 10px" type="text" name="nomsite" id="nomsite" required>
-            <label for="liensite">Lien du site : </label>
-            <input style="margin: 0 20px 0 10px" type="url" name="liensite" id="liensite" required>
-            <label for="logosite">Logo du site : </label>
-            <input style="margin: 0 20px 0 10px" type="url" name="logosite" id="logosite" required>
-            <label for="fluxrss">Flux RSS du site : </label>
-            <input style="margin: 0 20px 0 10px" type="url" name="fluxrss" id="fluxrss" required>
-            <input style="margin-left: 30px" type="submit" value="Ajouter le site">
-            <input type="hidden" name="action" value="ajouterSite">
-        </form>
+    <!-- Fenêtre modale -->
+    <div class="modal fade" id="addSiteModal" tabindex="-1" aria-labelledby="addSiteModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form class="modal-content" method="post">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addSiteModalLabel">Ajouter un flux RSS</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="col mb-3">
+                        <label for="nomsite">Nom du site :</label>
+                        <input class="form-control" type="text" name="nomsite" id="nomsite" required>
+                    </div>
+                    <div class="col mb-3">
+                        <label for="liensite">Lien du site :</label>
+                        <input class="form-control" type="url" name="liensite" id="liensite" required>
+                    </div>
+                    <div class="col mb-3">
+                        <label for="logosite">Logo du site :</label>
+                        <input class="form-control" type="url" name="logosite" id="logosite" required>
+                    </div>
+                    <div class="col">
+                        <label for="fluxrss">Flux RSS du site :</label>
+                        <input class="form-control" type="url" name="fluxrss" id="fluxrss" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="col">
+                        <input type="reset" class="btn btn-danger" value="Réinitialiser">
+                    </div>
+                    <input type="button" class="btn btn-secondary col-auto" data-bs-dismiss="modal" value="Fermer">
+                    <input type="submit" class="btn btn-primary col-auto" value="Ajouter">
+                </div>
+                <input type="hidden" name="action" value="ajouterSite">
+            </form>
+        </div>
     </div>
+
     <div class="m20-0">
         <h4 class="text-decoration-underline">Sites référencés</h4>
-        <ul class="listeSite m20-0">
+        <!-- Bouton de la fenêtre modale -->
+        <button type="button" class="btn btn-primary m-2" data-bs-toggle="modal" data-bs-target="#addSiteModal">Ajouter
+            un flux RSS
+        </button>
+        <ul class="listeSite">
             <?php
-            if(!empty($tabSites)){
+            if (!empty($tabSites)) {
                 foreach ($tabSites as $site) {
                     ?>
                     <li class="site-item">
@@ -71,9 +103,9 @@
                                 </a>
 
                             </div>
-                            <div class="col-sm-auto">
-                                <a style="align-self: end"
-                                   href="index.php?action=supprimerSite&idWebsite=<?php echo $site->getFluxRSS(); ?>">
+                            <div class="col-auto align-self-center">
+                                <a
+                                        href="index.php?action=supprimerSite&idWebsite=<?php echo $site->getFluxRSS(); ?>">
                                     <img width="24" height="24" src="Vues/assets/img/remove.png">
                                 </a>
                             </div>
@@ -81,8 +113,7 @@
                     </li>
                     <?php
                 }
-            }
-            else echo "<h3 class='text-center'>Aucune news à afficher</h3>";
+            } else echo "<h3 class='text-center'>Aucune news à afficher</h3>";
             ?>
         </ul>
     </div>
